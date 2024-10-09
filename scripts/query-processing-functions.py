6dfb32834d7b6f784df74d5af95b2d7201052e75
Leandro <123456@gmail.com>
2024-10-08 22:30:16 -0300

    parser

--- scripts/query-processing-functions.py
+++ scripts/query-processing-functions.py
@@ -1,4 +1,8 @@
+import re
+
 def parser(query):
+    # Ex: SELECT <field> from <table> where <field> = 10
+
     query = query.strip()
     if len(query) == 0:
         return False
@@ -7,23 +11,35 @@ def parser(query):
     
     state = "INITIAL"
     states = {
-        "INITIAL": ["SELECT"],
-        "SELECT": ["FROM", "WHERE", "GROUP BY", "HAVING", "ORDER BY"],
-        "FROM": ["WHERE", "GROUP BY", "HAVING", "ORDER BY"],
-        "WHERE": ["GROUP BY", "HAVING", "ORDER BY"],
-        "GROUP BY": ["HAVING", "ORDER BY"],
-        "HAVING": ["ORDER BY"],
-        "ORDER BY": []
+        "INITIAL":  [r"^SELECT$"],                                  # "SELECT"   
+        
+        "SELECT":   [r"^\*$",    r"^[A-Z].*"],                      # "*", LABEL
+
+        "FROM":     [r"^[A-Z].*"],                                  # LABEL
+
+        "WHERE":    [r"^\($",     r"^[A-Z].*",     r"^\d+$"],       # "(", LABEL, NUMBER
+
+        "LABEL":    [r"^SELECT$", r"^FROM$", r"^WHERE$"]
+
     }
 
+
+    def is_valid_word(word, state):
+        for i in states[state]:
+            if re.fullmatch(i, word):
+                return True    
+        return False
+
     for word in words:
-        if state not in states or word not in states[state]:
+        if state not in states or not is_valid_word(word, state):
             return False  # Erro de sintaxe
         state = word
 
-    return state == "ORDER BY"  # Assumimos que "ORDER BY" é o último estado válido
+    return True
 
-# Exemplo de uso
+
+
+# Exemplo de uso ------------------------------------------------------------------------------
 query1 = "SELECT * FROM customers WHERE country = 'USA'"
 
 print(parser(query1)) 

9e162c65fd12de70c45f44e9b198bd1ef6aabc63
theiv767 <davib.g767@gmail.com>
2024-11-05 20:43:24 -0300

    parser

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,10 +1,275 @@
 # VALIDAÇÃO DE SINTAXE
 
 import re
+import json
 from utils import *
+
+
+storage_tbls = json.load(open('scripts/tables.json'))
+#-----------------------------------------------------------------------------
+
+def get_num_match(word):
+    count = sum(
+        1 for subdict in storage_tbls.values()
+        if isinstance(subdict, dict) and word.upper() in (key.upper() for key in subdict.keys())
+    )
+
+    return count
+
+
+def is_valid_where(qry_where, alias):
+    if qry_where  == "":
+        return True, "ok"
+    
+    qry_where = get_query_vector(qry_where)
+
+    
+    operators = {
+        "relational_operators"  :   {
+            "characters" :["=", ">=", "<=", "<>", ">", "<", "="],
+            "acepted_types" : ["INT", "FLOAT", "STRING", "DATE"],
+            "return_type" : ["BOOL"]
+        },
+
+        "arithmetic_operators"   :   {
+            "characters" : ["+", "-", "*", "/"],
+            "acepted_types" : ["INT", "FLOAT", "STRING", "DATE"],
+            "return_type" : ["INT", "FLOAT", "STRING", "DATE"]
+        },
+
+        "Logical_operators"    :   {
+            "characters" : ["AND"],
+            "acepted_types" : ["BOOL"],
+            "return_type" : ["BOOL"]
+        }
+
+    }
+
+
+
+    current_types       =   []
+    current_operator    =   []
+
+
+    for idx, word in enumerate(qry_where):
+
+        if word == "(":
+            continue
+
+        if word == ")":
+            acept_operator = current_types[-1] in operators[current_operator[-1]]["acepted_types"]
+            acept_types = current_types[-1] == current_types[-2]
+            if acept_operator and acept_types:
+                if current_operator[-1] != "arithmetic_operators":
+                    current_types.pop()
+                    current_types.append(operators[current_operator[-1]]["return_type"])
+                current_operator.pop()
+            else:
+                return False, f"Objeto {word} possui um tipo inválido!!!"   
+
+        # operador -------------------------------------
+        for operator_type in operators.keys():
+            if word in operators[operator_type]["characters"]:
+                current_operator.append(operator_type)
+                continue    
+
+        # tabelas.coluna -------------------------------
+        if re.fullmatch(r"^[A-Z].*", word):
+            colum_type = ""
+            
+            if "." in word:
+                tbl_alias = word.split(".")[0]
+                tbl_colum = word.split(".")[1]
+
+                # ---------------
+                # validando o alias
+                table_name = ""
+                exist_alias = False
+                for i in alias:
+                     if i[0].upper() == tbl_alias.upper():
+                        exist_alias = True
+                        table_name = i[1]
+                        table_columns_upper = {k.upper() for k in storage_tbls[i[1]].keys()}
+                        if not tbl_colum.upper() in table_columns_upper:
+                            return False, f"Coluna '{tbl_colum}' não encontrada na tabela {i[1]}!!!"
+                        break
+                
+                if not exist_alias:
+                    return False, f"Alias '{tbl_alias}' não definido na consulta!!!"
+
+                #--------------
+                # verificação de tipagem
+                colum_type = storage_tbls[table_name][tbl_colum]
+
+                if "VARCHAR(" in colum_type:
+                    colum_type = "STRING"
+                elif "TINYINT" in colum_type:
+                    colum_type = "INT"
+                elif "DATE" in colum_type:
+                    colum_type = "DATE"
+                elif "DECIMAL(" in colum_type:
+                    colum_type = "FLOAT"
+                    
+            else:
+                tbl_colum = word
+
+                num_match = 0
+
+                table_name = ""
+                for i in alias:
+                    if tbl_colum in storage_tbls[1].keys():
+                        table_name = i[1]
+                        num_match += 1
+
+
+                if num_match > 1:
+                    return False, f"Tabela da coluna '{tbl_colum}' não foi especificada!!!" 
+                elif num_match < 1:
+                    return False, f"Tabela da coluna '{tbl_colum}' não existe entre as tabelas!!!" 
+                
+
+                colum_type = storage_tbls[table_name][tbl_colum]
+
+                if "VARCHAR(" in colum_type:
+                    colum_type = "STRING"
+                elif "TINYINT" in colum_type:
+                    colum_type = "INT"
+                elif "DATE" in colum_type:
+                    colum_type = "DATE"
+                elif "DECIMAL(" in colum_type:
+                    colum_type = "FLOAT"
+
+
+            is_last_parenthesis = False
+            if idx-1 >= 0:
+                if qry_where[idx-1] == "(":
+                    is_last_parenthesis = True
+            
+            if len(current_types) == 0 or is_last_parenthesis:
+                current_types.append(colum_type)
+            else:
+                acept_operator = colum_type in operators[current_operator[-1]]["acepted_types"]
+                acept_types = current_types[-1] == colum_type
+                if acept_operator and acept_types:
+                    if current_operator[-1] != "arithmetic_operators":
+                        current_types.pop()
+                        current_types.append(operators[current_operator[-1]]["return_type"])
+                    current_operator.pop()
+                else:
+                    return False, f"Objeto {word} possui um tipo inválido!!!"
+
+            continue
+    
+    if len(current_types)!= 1 and "BOOL" != current_types[-1]:
+        return False, "Where espera um booleano!!!"
+    
+    return True, "ok"
+
+
+
+def is_valid_rels(query):
+    query = query.upper()
+    storage_tbls = json.load(open('scripts/tables.json'))
+
+    print(query)
+
+    qry_select = query.split("FROM")[0]
+    print(qry_select)
+    qry_tables = "FROM"+query.split("FROM")[1]
+    
+    qry_where = ""
+    if " WHERE " in query:
+        qry_where = qry_tables.split("WHERE")[1]
+        qry_tables = qry_tables.split("WHERE")[0]
+
+    qry_select  = qry_select.strip()
+    qry_tables  = qry_tables.strip()
+    qry_where   = qry_where.strip()
+
+    alias = []
+    
+    # Regex para capturar o padrão "FROM <tabela> <alias>" e "JOIN <tabela> <alias>"
+    #pattern = r"\b(?:FROM|JOIN)\s+(\w+)\s+(\w+)"
+    #matches = re.findall(pattern, qry_tables, re.IGNORECASE)
+    
+    # alias é uma tabela que, em cada posição, guarda (alias, tabela_correspondente)
+    #alias = [(alias, table) for table, alias in matches]
+
+    pattern = r"\b(?:FROM|JOIN)\s+(\w+)(?:\s+(\w+))?"
+    matches = re.findall(pattern, qry_tables, re.IGNORECASE)
+
+    # Constrói a lista de alias, usando o próprio nome da tabela como alias quando ele não é especificado
+    alias = [(alias if alias else table, table) for table, alias in matches]
+
+
+
+    for i in alias:
+        if not i[1].upper() in {k.upper() for k in storage_tbls.keys()}:
+            return False, f"Tabela '{i[1]}' não encontrada!!!"
+
+
+    separators =   ["=", ">=", "<=", "<>", ",", "(", ")", "*"]
+
+
+    qry_select = get_query_vector(qry_select)
+    qry_select = qry_select[1:]
+    for word in qry_select:
+        if word in separators:
+            continue
+
+        if re.fullmatch(r"^[A-Z].*", word):
+            if "." in word:
+                tbl_alias = word.split(".")[0]
+                tbl_colum = word.split(".")[1]
+
+                for i in alias:
+                    if i[0].upper() == tbl_alias.upper():
+                        table_columns_upper = {k.upper() for k in storage_tbls[i[1]].keys()}
+                        if not tbl_colum.upper() in table_columns_upper:
+                            return False, f"Coluna '{tbl_colum}' não encontrada na tabela {i[1]}!!!"
+                
+            else:
+                tbl_colum = word
+                num_match = get_num_match(word)
+
+                if num_match > 1:
+                    return False, f"Tabela da coluna '{tbl_colum}' não foi especificada!!!"  
+
+
+
+    # ADICIONAR VALIDAÇÃO DE WHERE ---------------------------------------
+    if qry_where != "":
+        valid_where, msg = is_valid_where(qry_where, alias)            
+        if not valid_where:
+            return valid_where, msg
+
+
+
+    return True, "ok"
+
+
+
+
+        
+        
+
+    
+
+
+
+    
+
+
 
 def parser(query):
     # Ex: SELECT <field> from <table> where <field> = 10
+
+    select_regex = r"SELECT\s+(.*?)\s+FROM\s+(\w+)(?:\s+JOIN\s+(\w+)\s+ON\s+.*?)*"
+    where_regex = r"WHERE\s+(.*)"
+    
+    match = re.search(select_regex, query, re.IGNORECASE)
+    if not match:
+        return False, "Teste inicial"
 
     query = query.strip()
     if len(query) == 0:
@@ -63,7 +328,6 @@ def parser(query):
     num_open_parenthesis = 0
     num_comparations = 0
 
-    print(words)
     for idx_word, word in enumerate(words):
         if words[idx_word] in list(states.keys()):
             state = word
@@ -99,7 +363,11 @@ def parser(query):
         if end_word:
             if end_prev_word:
                 if not word == ")":
-                    return False, f"A palavra {words[idx_word-1]} não espera {word}"
+                    if not state in ["FROM", "JOIN"]:
+                        return False, f"A palavra {words[idx_word-1]} não espera {word}"
+                    else:
+                        if words[idx_word-2] != state:
+                            return False, f"A palavra {words[idx_word-1]} não espera {word}"            
             
             if words[idx_word-1] in comparators:
                 num_comparations -= 1      
@@ -127,12 +395,13 @@ def parser(query):
 
             if num_open_parenthesis != 0:
                 return False, f"Parêntesis ficaram abertos!!!"
-         
+
+
+    valid_rels, msg_rels = is_valid_rels(query)
+    if not valid_rels:
+        return False, msg_rels
+    
 
     return True, "Sintax válida!"
 
-
-
-print(parser('select *, asdf, ff from asdf'))
-
-
+print( parser("select * from endereco ed WHERE ed.idEndereco = ed.idEndereco ") )
